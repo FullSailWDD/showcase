4dfab93f3b603110b81f7875a1a3a4866bcda5eb
ncarvalho <nicolecar14@gmail.com>
2015-08-06 01:47:13 -0400

    changes castr videos

--- js/reveal.js
+++ js/reveal.js
@@ -141,7 +141,7 @@
 			parallaxBackgroundVertical: null,
 
 			// Number of slides away from the current that are visible
-			viewDistance: 3,
+			viewDistance: 1,
 
 			// Script dependencies to load
 			dependencies: []
